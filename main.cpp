a51702bea17fc97b7d0367207d2fced30849bcee
OldScripter <Oldscripter@gmail.com>
2021-06-18 01:25:21 +0300

    Word search function is added

--- main.cpp
+++ main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
+#include <fstream>
+
+int GetWordQuantity(std::string word)
+{
+    char* dir = "C:\\Users\\oldsc\\CLionProjects\\Task_19_1\\input.txt";
+    std::ifstream reading_stream(dir);
+
+    int counter = 0;
+    while (!reading_stream.eof())
+    {
+        std::string buffer;
+        reading_stream >> buffer;
+        if (buffer == word)
+            counter++;
+    }
+
+    reading_stream.close();
+    return counter;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::string word_to_find;
+    std::cout << "Please enter the word to find: ";
+    std::cin >> word_to_find;
+
+    std::cout << "Quantity of words: " << GetWordQuantity(word_to_find) << "\n";
+
     return 0;
 }
